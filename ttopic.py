03965b02d3342418d24425b04d526842d98fa360
midnight <jw79985@gmail.com>
2024-03-30 21:07:58 +0100

    User input moved to Group class

--- ttopic.py
+++ ttopic.py
@@ -9,19 +9,10 @@ async def main():
     # Connect to group
     await group.connect()
 
-    # Get your Topics list
-    topic_id = None
-    list_of_topics = await group.forum_topics()
-    if list_of_topics:
-        while True:
-            index = input(f"Choose which topic you want to download (0-{len(list_of_topics) - 1}): ")
-            if index.isnumeric():
-                topic_index = int(index)
-                if 0 <= topic_index <= len(list_of_topics) - 1:
-                    topic_id, topic_title = list_of_topics[topic_index]
-                    break
+    # Get topic ID and Image list
+    media_list = await group.topic(await group.input())
 
-    media_list = await group.topic(topic_id)
+    # Downloader..
     input(f"\nPress enter to start the download ")
     await group.telegram.downloader(media_list)
     loop.stop()
